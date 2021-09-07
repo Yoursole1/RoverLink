05dbfdd2327fdb622d6c3e3be05c0303b69f441e
Yoursole1 <80355246+Yoursole1@users.noreply.github.com>
2021-09-06 20:48:02 -0700

    Update main.java

--- RoverLink/src/main/java/main/main.java
+++ RoverLink/src/main/java/main/main.java
@@ -1,3 +1,5 @@
+//THIS IS AN EXAMPLE IMPLEMENTATION -- If you are planning on using this library please delete this file and compile the rest into a shaded jar
+
 package main;
 
 import java.io.IOException;
@@ -8,6 +10,8 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] a) throws IOException {
 
+        
+        
         RoverLink link = new RoverLink(InetAddress.getByName("10.0.0.195"),"login");
 
 
